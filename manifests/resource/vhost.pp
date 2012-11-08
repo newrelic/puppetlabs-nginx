5f5b167d1a759adede2dfc7d0f293d14b6c991dc
Jonathan Thurman <jthurman@newrelic.com>
2012-11-08 11:33:45 -0800

    create an empty array by default, not a hash...

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -37,7 +37,7 @@
 #  }
 define nginx::resource::vhost(
   $server_name        = $name,
-  $server_aliases     = { },
+  $server_aliases     = [ ],
   $ensure             = 'enable',
   $listen_ip          = '*',
   $listen_port        = '80',
